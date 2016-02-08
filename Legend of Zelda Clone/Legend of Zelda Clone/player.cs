d4c732b1c503fe15793acdd4182279682f37001f
Barney Harris <tangeandfwist@gmail.com>
2016-02-08 19:33:00 +0000

    Transition Fix and Tidy-up
    - Switched from storing all versions of project on latest version of
    repo to just he most recent one.
    
    - Restored zone transitions to 16 and 11, rather than 15 and10.
    
    - Traced collision error to float->int conversion truncation, added
    rounding to viewport final value in zone transition

---
+++ Legend of Zelda Clone/Legend of Zelda Clone/player.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Legend_of_Zelda_Clone
+{
+    class player
+    {
+
+        private Vector2 position;
+        private int health;
+        private int maxHealth;
+
+        public player(Vector2 pos, int hp)
+        {
+            //health = hearts * 2, because damage = half heart.
+            position = pos;
+            maxHealth = hp;
+            health = maxHealth;
+        }
+
+        public void getHeart()
+        { health++; }
+
+        public void HPup()
+        { maxHealth += 2; }
+
+        public void damage(int amount)
+        { health -= amount; }
+
+        public void move(Vector2 offset)
+        {
+            position += offset;
+
+            position.X = (float)Math.Round(position.X, 3);
+            position.Y = (float)Math.Round(position.Y, 3);
+
+        }
+
+        public int getHealth()
+        { return health; }
+
+        public Vector2 getPos()
+        { return position; }
+        
+    }
+}
